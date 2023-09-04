31bb4c3ba990844a38f3682ce3b5994978f9d898
sardor <sardorbeknortojiyev3501@gmail.com>
2023-09-04 07:01:04 -0700

    finished EntranceActivity

--- app/src/main/java/uz/ruzibekov/multiplymaster/ui/entrance/EntranceActivity.kt
+++ app/src/main/java/uz/ruzibekov/multiplymaster/ui/entrance/EntranceActivity.kt
@@ -1,13 +1,14 @@
-package uz.ruzibekov.multiplymaster
+package uz.ruzibekov.multiplymaster.ui.entrance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.ruzibekov.multiplymaster.R
 
-class MainActivity : AppCompatActivity() {
+class EntranceActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_entrance)
 
     }
 }
