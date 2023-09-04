588d8c1d4600acab4094378aa96a686b995521e4
sardor <sardorbeknortojiyev3501@gmail.com>
2023-09-04 10:03:21 -0700

    other changes

--- app/src/main/java/uz/ruzibekov/multiplymaster/ui/entrance/EntranceActivity.kt
+++ app/src/main/java/uz/ruzibekov/multiplymaster/ui/entrance/EntranceActivity.kt
@@ -1,14 +1,35 @@
 package uz.ruzibekov.multiplymaster.ui.entrance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import uz.ruzibekov.multiplymaster.R
+import uz.ruzibekov.multiplymaster.ui.home.HomeActivity
 
 class EntranceActivity : AppCompatActivity() {
+
+    private var btnStart: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_entrance)
 
+        initViews()
+
+        initListeners()
+    }
+
+    private fun initViews() {
+        btnStart = findViewById(R.id.btn_start)
+    }
+
+    private fun initListeners(){
+
+        btnStart?.setOnClickListener {
+
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/uz/ruzibekov/multiplymaster/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package uz.ruzibekov.multiplymaster.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.ruzibekov.multiplymaster.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
