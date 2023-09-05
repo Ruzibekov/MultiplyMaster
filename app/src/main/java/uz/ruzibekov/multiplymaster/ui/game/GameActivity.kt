445b0a39f44271e0c557273a2955acdeb4d15302
sardor <sardorbeknortojiyev3501@gmail.com>
2023-09-05 02:26:22 -0700

    add activity_game.xml

--- app/src/main/java/uz/ruzibekov/multiplymaster/ui/game/GameActivity.kt
+++ app/src/main/java/uz/ruzibekov/multiplymaster/ui/game/GameActivity.kt
@@ -2,9 +2,12 @@ package uz.ruzibekov.multiplymaster.ui.game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import uz.ruzibekov.multiplymaster.R
 
 class GameActivity : AppCompatActivity() {
+
+    private var btnBack: ImageButton? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
